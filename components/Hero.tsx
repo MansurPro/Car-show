5057f83a1e7ccb8bac02be24e1883eeb69015b25
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-07-29 10:04:49 -0500

    Hero finished - Navbar and footer starting

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,6 +34,8 @@ const Hero = () => {
             fill
             className='object-contain'
           />
+
+          <div className='hero__image-overlay' />
         </div>
       </div>
     </div>
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from "./Hero";
 import GustomButton from './GustomButton';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export {
     Hero,
     GustomButton,
+    Navbar,
+    Footer
 }
