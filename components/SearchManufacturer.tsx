e278ab3ea9b16f615f687863fdb03185fdeb4b8d
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-08-21 22:26:47 +0600

    Search Manufacturers filter adding

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,50 @@
-import { SearchManufacturerProps } from "@/types";
+"use client";
 
+import { useState, Fragment } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import { SearchManufacturerProps } from "@/types";
+import Image from "next/image";
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState('');
+
+  
+
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image 
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input 
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options>
+
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
   )
 }
 
