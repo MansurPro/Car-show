99226e25f17e9e60f3cdc30e6e121430a0007e5b
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-08-31 08:45:52 -0500

    Car Details - Dialog

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -95,7 +95,11 @@ const CarCard = ({ car }: CarCardProps) => {
         </div>
       </div>
 
-      <CarDetails />
+      <CarDetails 
+        isOpen={isOpen} 
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   )
 }
