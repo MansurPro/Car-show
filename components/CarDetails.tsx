dc71aa498a7878efcca13be1b1f7786f5c7cdb5c
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-09-03 13:51:07 -0500

    Car ditails page finished

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -40,7 +40,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom='opacity-100 scale-100'
                 leaveTo='opacity-0 scale-95'
               >
-                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type='button'
                     className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
@@ -97,7 +97,24 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     </div>
                   </div>
 
-                  
+                  <div className='flex-1 flex flex-col gap-2'>
+                    <h2 className='font-semibold text-xl capitalize'>
+                      {car.make} {car.model}
+                    </h2>
+
+                    <div className='mt-3 flex flex-wrap gap-4'>
+                      {Object.entries(car).map(([key, value]) => (
+                        <div className='flex justify-between gap-5 w-full text-right' key={key}>
+                          <h4 className='text-gray capitalize'>
+                            {key.split("_").join(" ")}
+                          </h4>
+                          <p className='text-black-100 font-semibold'>
+                            {value}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
