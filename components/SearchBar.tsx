b00f985965e82ae17851b359b83699ee33f9e8ad
Mansurbek Satarov <mansurbek1203@gmail.com>
2023-09-27 20:06:46 -0500

    fixed the scrolling bug

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -49,7 +49,7 @@ const SearchBar = () => {
 
     const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
-    router.push(newPathname);
+    router.push(newPathname, {scroll: false});
   }
 
   return (
